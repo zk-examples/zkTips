3afc1db305f19f8f446de1ec65380ae56ce89da7
Sergey <ssobolev@ptsecurity.com>
2025-04-04 15:42:11 +0300

    init zkTips

---
+++ test/zkTips.ts
@@ -0,0 +1,8 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("zkTips", function () {});
